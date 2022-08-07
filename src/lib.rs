c6a71dcfb8fec65d9c9d7bd0c7d5dae6288321dd
steph-mipt <siniakov.se@phystech.edu>
2022-08-07 13:36:11 +0300

    add verification + trie data

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,32 @@ pub use self::cli::Args;
 use actix_web::{web::{self, Json}, App, HttpServer};
 use types::{VerkleReq, VerkleResp};
 
+use ark_serialize::{CanonicalSerialize};
+
 async fn get_block_info(data: Json<VerkleReq>) -> Result<Json<VerkleResp>, actix_web::Error> {
     let block_number = data.into_inner().block_number;
     let block_rlp = verkle::get_rlp(block_number).await.unwrap();
+
+    verkle::save_rlp(block_rlp.clone(), block_number).await.unwrap();
+
+    let block = verkle::decode_block(block_rlp.clone()).unwrap();
+
+    verkle::print_block_info(&block);
+
+    // for 462198 (dec)
+    let info = verkle::verification(block, "4ab9e47a3b8508c264b56bca01a82ff0fa01dddb8e97d012058c49e133850539".to_owned());
+
+    for (path, comm) in info.unwrap().commitments_by_path {
+        println!("\tnode");
+        let mut v = Vec::new();
+        comm.serialize(&mut v).unwrap();
+        println!("comm :\t{:?}", hex::encode(v));
+        print!("path :\t");
+        for el in path {
+            print!("{:#02x} ", el);
+        }
+        println!();
+    }
 
     Ok(Json(VerkleResp { block_rlp }))
 }
