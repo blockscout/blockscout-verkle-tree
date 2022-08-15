06e26df2bebf0ce3dd07df601038057b00624c89
steph-mipt <siniakov.se@phystech.edu>
2022-08-15 19:56:09 +0300

    added base commitments

--- src/dot/mod.rs
+++ src/dot/mod.rs
@@ -1,15 +1,54 @@
 use verkle_trie::proof::UpdateHint;
 use ark_serialize::{CanonicalSerialize};
+// use verkle_trie::EdwardsProjective;
+use std::borrow::Cow;
 
 use std::io::Write;
 
 type Nd = usize;
-type Ed<'a> = &'a (usize, usize);
-struct Graph { nodes: Vec<&'static str>, edges: Vec<(usize,usize)> }
+type Ed<'a> = &'a (usize, usize, u8);
+struct Graph { nodes: Vec<String>, edges: Vec<(usize,usize,u8)> }
 
-pub fn render_to<W: Write>(output: &mut W) {
-    let nodes = vec!("{x,y}","{x}","{y}","{}");
-    let edges = vec!((0,1), (0,2), (1,3), (2,3));
+
+pub fn render_to<W: Write>(output: &mut W, data: &UpdateHint) {
+    let mut nodes = vec![];
+    let mut previous_items = Vec::<(Vec::<u8>, String)>::new();
+    let mut edges = vec![];
+    let mut my_index = 0;
+
+    for (path, comm) in data.commitments_by_path.clone() {
+        let mut v_tmp = vec![];
+        let _res = CanonicalSerialize::serialize(&comm, &mut v_tmp);
+        let commitment_in_hex: String = format!("0x{}", hex::encode(v_tmp));
+        nodes.push(commitment_in_hex.clone());
+
+        for (index, item) in previous_items.iter().enumerate() {
+            let len_current = item.0.len();
+
+            if path.len() == len_current + 1 {
+                let common = path.iter().zip(&item.0).filter(|&(x, y)| x == y).count();
+
+                if common == len_current {
+                    edges.push((index, my_index, path[path.len() - 1]));
+                }
+            }
+        }
+        previous_items.push((path, commitment_in_hex));
+        my_index += 1;
+    }
+
+    for (comm, val2) in data.depths_and_ext_by_stem.clone() {
+        let commitment_in_hex = format!("0x{}", hex::encode(comm));
+
+        // пройти по префиксному дереву по commitment_in_hex на depth вершин
+        // и записать в nodes
+        println!("commitment : {}", commitment_in_hex);
+        println!("ext and depth : {:?}", val2);
+    }
+
+    // пройтись по всем keys и записать их в дерево, если есть ext ноды !
+    
+
     let graph = Graph { nodes: nodes, edges: edges };
 
     dot::render(&graph, output).unwrap()
@@ -21,10 +60,10 @@ impl<'a> dot::Labeller<'a, Nd, Ed<'a>> for Graph {
         dot::Id::new(format!("N{}", n)).unwrap()
     }
     fn node_label<'b>(&'b self, n: &Nd) -> dot::LabelText<'b> {
-        dot::LabelText::LabelStr(self.nodes[*n].into())
+        dot::LabelText::LabelStr(self.nodes.get(*n).unwrap().into())
     }
-    fn edge_label<'b>(&'b self, _: &Ed) -> dot::LabelText<'b> {
-        dot::LabelText::LabelStr("lalala".into())
+    fn edge_label<'b>(&'b self, edge: &Ed) -> dot::LabelText<'b> {
+        dot::LabelText::LabelStr(Cow::Owned(format!("{:0x}", edge.2)))
     }
 }
 
@@ -38,7 +77,7 @@ impl<'a> dot::GraphWalk<'a, Nd, Ed<'a>> for Graph {
 pub fn to_dot(uh: &UpdateHint) {
     use std::fs::File;
     let mut f = File::create("example3.dot").unwrap();
-    render_to(&mut f);
+    render_to(&mut f, &uh);
 
     for (path, comm) in uh.commitments_by_path.clone() {
         println!("\tnode");
@@ -57,6 +96,4 @@ pub fn to_dot(uh: &UpdateHint) {
         println!("val1 : {}", hex::encode(val1));
         println!("val2 : {:?}", val2);
     }
-    // println!("extra-info2:\n\n");
-    // println!("{:?}", val.other_stems_by_prefix);
 }
